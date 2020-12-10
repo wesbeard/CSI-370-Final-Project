b3aa43c62641395fc6cfb125d697586d933f3fed
Wes Beard <wesley.beard@mymail.champlain.edu>
2020-12-10 18:18:43 -0500

    Add project files.

---
+++ CSI-370 Final Project/main.cpp
@@ -0,0 +1,28 @@
+/*
+	Wes Beard
+	CSI-370
+	11/12/2020
+	Assignment 9
+*/
+
+#include <iostream>
+
+using namespace std;
+
+bool gameOver = false;
+int  dimensions = 10;
+
+void printBoard() {
+	cout << "Printing board..." << endl;
+}
+
+void generateBoard() {
+	cout << "Generating board..." << endl;
+ }
+
+int main() {
+
+	generateBoard();
+
+	
+}
