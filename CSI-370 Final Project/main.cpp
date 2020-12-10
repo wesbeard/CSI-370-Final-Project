9588280514163555898ea6b7c723e66e4c2af057
Wes Beard <wesley.beard@mymail.champlain.edu>
2020-12-10 18:40:40 -0500

    Basic game logic

--- CSI-370 Final Project/main.cpp
+++ CSI-370 Final Project/main.cpp
@@ -2,8 +2,8 @@
 	Wes Beard - wesley.beard@mymail.champlain.edu
 	Michael Leonard - michael.leonard@mymail.champlain.edu
 	CSI-370
-	12/12/2020
-	Assignment 9
+	12/14/2020
+	Final Project
 */
 
 #include <iostream>
@@ -12,6 +12,8 @@ using namespace std;
 
 bool gameOver = false;
 int  dimensions = 10;
+int  spaceX;
+int  spaceY;
 
 void printBoard() {
 	cout << "Printing board..." << endl;
@@ -21,9 +23,20 @@ void generateBoard() {
 	cout << "Generating board..." << endl;
  }
 
+void validateInput() {
+	cout << "Enter the X coordinate of the desired space: " << endl;
+	cin >> spaceX;
+	cout << "Enter the Y coordinate of the desired space: " << endl;
+	cin >> spaceY;
+}
+
 int main() {
 
 	generateBoard();
+	while (!gameOver) {
+		printBoard();
+		validateInput();
+	}
 
-	
+	return 0;
 }
