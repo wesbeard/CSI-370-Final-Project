d6a8cbb6d06097d7d73e79ac77b122d22aae50a1
Wes Beard <wesley.beard@mymail.champlain.edu>
2020-12-10 19:15:18 -0500

    Working on generation

--- CSI-370 Final Project/main.cpp
+++ CSI-370 Final Project/main.cpp
@@ -7,36 +7,126 @@
 */
 
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
+int i;
+int j;
+int spaceX;
+int spaceY;
+int minesCount;
 bool gameOver = false;
-int  dimensions = 10;
-int  spaceX;
-int  spaceY;
+const int dimensions = 10;
+int board[dimensions][dimensions];
 
 void printBoard() {
-	cout << "Printing board..." << endl;
-	cout << "yo" << endl;
+
+	for (i = 0; i < dimensions; i++) {
+		for (j = 0; j < dimensions; j++) {
+			cout << " - " << board[i][j];
+		}
+		cout << endl;
+	}
 }
 
 void generateBoard() {
+
 	cout << "Generating board..." << endl;
+
+	for (i = 0; i < dimensions; i++) {
+		for (j = 0; j < dimensions; j++) {
+			int random = rand() % 10;
+			if (random == 9) {
+				board[i][j] = 9;
+			}
+			else {
+				board[i][j] = 0;
+			}
+		}
+	}
+
+	for (i = 0; i < dimensions; i++) {
+		for (j = 0; j < dimensions; j++) {
+			minesCount = 0;
+			if (board[i][j] == 9) {
+				if (board[i - 1][j - 1] != 9) {
+					board[i - 1][j - 1]++;
+				}
+				if (board[i - 1][j + 1] != 9) {
+					cout << board[i - 1][j + 1] << endl;
+					board[i - 1][j + 1]++;
+				}
+				if (board[i + 1][j - 1] != 9) {
+					board[i + 1][j - 1]++;
+				}
+				if (board[i + 1][j + 1] != 9) {
+					board[i + 1][j + 1]++;
+				}
+				if (board[i][j + 1] != 9) {
+					board[i][j + 1]++;
+				}
+				if (board[i][j - 1] != 9) {
+					board[i][j - 1]++;
+				}
+				if (board[i - 1][j] != 9) {
+					board[i - 1][j]++;
+				}
+				if (board[i + 1][j] != 9) {
+					board[i + 1][j]++;
+				}
+			}
+
+			/*if (board[i][j] == 0) {
+				if (board[i - 1][j - 1] == 9) {
+					minesCount++;
+				}
+				if (board[i - 1][j + 1] == 9) {
+					minesCount++;
+				}
+				if (board[i + 1][j + 1] == 9) {
+					minesCount++;
+				}
+				if (board[i + 1][j - 1] == 9) {
+					minesCount++;
+				}
+				if (board[i][j - 1] == 9) {
+					minesCount++;
+				}
+				if (board[i][j + 1] == 9) {
+					minesCount++;
+				}
+				if (board[i + 1][j] == 9) {
+					minesCount++;
+				}
+				if (board[i - 1][j] == 9) {
+					minesCount++;
+				}
+				board[i][j] = minesCount;
+			}*/
+		}
+	}
  }
 
-void validateInput() {
+bool validateInput() {
+
 	cout << "Enter the X coordinate of the desired space: " << endl;
 	cin >> spaceX;
 	cout << "Enter the Y coordinate of the desired space: " << endl;
 	cin >> spaceY;
+	return false;
 }
 
 int main() {
 
+	srand(time(NULL));
 	generateBoard();
+	printBoard();
 	while (!gameOver) {
-		printBoard();
-		validateInput();
+		if (validateInput()) {
+			printBoard();
+		}
 	}
 
 	return 0;
